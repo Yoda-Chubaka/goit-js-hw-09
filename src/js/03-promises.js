dcc762197bedad87050a7a30484e3bd17428850b
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-07-06 18:21:18 +0300

    My forth commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,98 +1,38 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-document.body.style.backgroundColor = 'yellowgreen';
-const form = document.querySelector('form.form');
-const options = {
-  position: 'center-bottom',
-  distance: '15px',
-  borderRadius: '15px',
-  timeout: 10000,
-  clickToClose: true,
-  cssAnimationStyle: 'from-right',
-};
+document.body.style.backgroundColor = "yellowgreen";
+const formRef = document.querySelector('.form');
 
-form.addEventListener('click', onPromiseCreate);
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(formRef.delay.value);
+
+  for (let i = 1; i <= formRef.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formRef.step.value);
+  }
+}
 
 function createPromise(position, delay) {
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve(obj);
       } else {
-        reject({ position, delay });
+        reject(obj);
       }
     }, delay);
   });
 }
-
-function onPromiseCreate(e) {
-  e.preventDefault();
-  const { delay, step, amount } = e.currentTarget.elements;
-  let inputDelay = Number(delay.value);
-  let inputStep = Number(step.value);
-  let inputAmount = Number(amount.value);
-
-  for (let i = 1; i <= inputAmount; i += 1) {
-    inputDelay += inputStep;
-
-    createPromise(i, inputDelay)
-      .then(({ position, delay }) => {
-        Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`,
-          options
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`,
-          options
-        );
-      });
-    e.currentTarget.reset();
-  }
-}
-
-// const refs = {
-//   body: document.querySelector('body'),
-//   form: document.querySelector('form.form'),
-//   delay: document.querySelector('[name="delay"]'),
-//   step: document.querySelector('[name="step"]'),
-//   amount: document.querySelector('[name="amount"]'),
-// }
-
-// refs.body.style.backgroundColor = 'yellowgreen';
-// refs.form.addEventListener('click', onPromiseCreate);
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// function onPromiseCreate(e) {
-//   e.preventDefault();
-
-//   let valueDelay = Number(refs.delay.value);
-//   let step = Number(refs.step.value);
-//   let amount = Number(refs.amount.value);
-
-//   for (let i = 1; i <= amount; i += 1) {
-//     let promiseDelay = valueDelay + step * i;
-
-//     createPromise(i, promiseDelay)
-//       .then(({ position, delay }) => {
-//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-//   }
-// }
